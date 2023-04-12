3c304d21220f81da96950687cec5881ef2acf5ef
DylanVallejo <dylan1811vallejo@gmail.com>
2023-04-11 23:31:14 -0500

    added section Sound and added fucntion to navigate to this section in the component jumbotron

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import Iphone from "../assets/images/iphone-14.jpg"
 import HoldingIphone from "../assets/images/iphone-hand.png"
 function Jumbotron() {
+    
+    const handleLearnMore = () => {
+        const element = document.querySelector(".sound-section");
+        window.scrollTo({
+            top: element?.getBoundingClientRect().top,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+    
     return (
         <div className='jumbotron-section wrapper'>
             <h2 className='title'>
@@ -21,7 +31,8 @@ function Jumbotron() {
                     </button>
                 </li>
                 <li>
-                    <a className='link'>
+                    {/* add a smooth aniamation */}
+                    <a className='link' onClick={handleLearnMore}>
                         Learn more
                     </a>
                 </li>
