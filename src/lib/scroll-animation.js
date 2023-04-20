37f6402460ae33080069996dfb1d57564fc888be
DylanVallejo <dylan1811vallejo@gmail.com>
2023-04-19 23:47:26 -0500

    added postions for x,yand z to animate the 3d image

--- src/lib/scroll-animation.js
+++ src/lib/scroll-animation.js
@@ -7,11 +7,34 @@ export const scrollAnimation = (positon, target, onUpdate) => {
     
     // defining properties to animate
     tl.to(positon,{
+        // values to change 
+        x: -3.38,
+        y: -10.74,
+        z: -5.93,
         scrollTrigger: {
             trigger: '.sound-section',
             star: "top bottom",
-            end: "top top"
-        }
+            end: "top top",
+            scrub: 2,
+            // upgrading performance
+            immediateRender: false
+        } ,
+        onUpdate
+    })
+    .to(target,{
+        // values to change 
+        x: 1.52,
+        y: 0.77,
+        z: -1.08,
+        scrollTrigger: {
+            trigger: '.sound-section',
+            star: "top bottom",
+            end: "top top",
+            scrub: 2,
+            // upgrading performance
+            immediateRender: false
+        } ,
+
     })
     
 }
